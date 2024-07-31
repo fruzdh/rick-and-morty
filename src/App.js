9e326d2f83f01aa8637f2efe82ad57d6bd844163
fruzdh <abrarnadip123@gmail.com>
2024-07-31 09:23:08 +0700

    feat: add react-gh-pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { Box } from '@chakra-ui/react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import CharacterDetail from './pages/CharacterDetail';
 import Home from './pages/Home';
 import LocationDetail from './pages/LocationDetail';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Box h={'100vh'} w={'100vw'} bgColor={'#FFFFBE'} overflowY={'scroll'} display={'flex'} justifyContent={'center'}>
         <Box maxH={'100vh'} maxW={'1280px'} p={'10px'}>
           <Routes>
@@ -17,7 +17,7 @@ function App() {
           </Routes>
         </Box>
       </Box>
-    </Router>
+    </HashRouter>
   );
 }
 
